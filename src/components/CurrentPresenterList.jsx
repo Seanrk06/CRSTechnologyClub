01567b5ae1fcaee2c2b98c2af3cadc632eedd97f
Sean Klein <87321511+Seanrk06@users.noreply.github.com>
2022-10-03 20:53:37 -0400

    Update CurrentPresenterList.jsx

--- src/components/CurrentPresenterList.jsx
+++ src/components/CurrentPresenterList.jsx
@@ -8,18 +8,18 @@ function CurrentPresenterList() {
           Melvin Jose
         </p>
         <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
-          On October 13, at 2:20 in room A13 we plan to have our second
-          presenter Melvin Jose! Mr. Hose has held a multitude of positions
+          On October 13th, at 2:20 in room A13, we plan to have our second
+          presenter Melvin Jose! Mr. Jose has held a multitude of positions
           across many internationally renowned companies. He recently got his
-          B.S in Chemical Engineering from Drexel University within which he
+          B.S. in Chemical Engineering from Drexel University during which he
           completed a Co-Op program with Johnson & Johnson in their consumer
-          healthcare division developing products. In 2017 he also did a Co-Op
+          healthcare division. In 2017, he also did a Co-Op
           program in the Janssen Pharmaceutical Companies of Johnson & Johnson
           conducting Biologics Research. He is currently a project engineer at
           Catalent Pharma Solutions linked{" "}
           <a href="https://www.catalent.com/" className="underline">
             here
-          </a>{" "}
+          </a>,
           which is the #1 partner in helping pharmaceutical, biotech, and
           consumer health innovators develop, deliver, and supply superior
           products that improve people's lives.
